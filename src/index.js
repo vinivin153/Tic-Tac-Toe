6137eca2ca472b2bbd613bf994200e7b503e1eb9
vinivin153 <vinivin153@naver.com>
2022-12-02 22:57:51 +0900

    feat: add onClickHandler

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import { React, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Square({ idx }) {
-  return <button className="square">{idx}</button>;
+function Square({ value, idx, onClickHandler }) {
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        onClickHandler(idx);
+      }}
+    >
+      {value}
+    </button>
+  );
 }
 
-function Board() {
+function Board({ squares, onClickHandler }) {
   const render = () => {
     const board = [];
 
     for (let idx = 0; idx < 9; idx++) {
-      board.push(<Square idx={idx} />);
+      board.push(
+        <Square
+          key={idx}
+          idx={idx}
+          value={squares[idx]}
+          onClickHandler={onClickHandler}
+        />
+      );
     }
 
     return board;
@@ -19,6 +35,23 @@ function Board() {
   return <div className="board">{render()}</div>;
 }
 
+function TicTacToeGame() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const onClickHandler = (idx) => {
+    const newSquares = [...squares];
+
+    newSquares[idx] = idx;
+    setSquares(newSquares);
+  };
+
+  return (
+    <div>
+      <Board squares={squares} onClickHandler={onClickHandler} />
+    </div>
+  );
+}
+
 // rendering
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Board />);
+root.render(<TicTacToeGame />);
