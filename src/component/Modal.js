8075b413807e3a9011ca2c786b425c4a2e8d9623
vinivin153 <vinivin153@naver.com>
2022-12-08 15:12:19 +0900

    feat: add modal

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -1,10 +1,21 @@
-export default function Modal({ winner, regameHandler }) {
+export default function Modal({ winner, regameHandler, closeHandler }) {
   return (
     <div className="modal">
-      <div className="winner">{winner}의 승리!</div>
-      <button className="regame" onClick={regameHandler}>
-        게임 재시작하기
-      </button>
+      <div className="box-modal">
+        <header>
+          <button className="btn-close" onClick={closeHandler}>
+            X
+          </button>
+        </header>
+        <section>
+          <div className="winner">{winner}의 승리!</div>
+        </section>
+        <footer>
+          <button className="btn-regame" onClick={regameHandler}>
+            게임 재시작하기
+          </button>
+        </footer>
+      </div>
     </div>
   );
 }
